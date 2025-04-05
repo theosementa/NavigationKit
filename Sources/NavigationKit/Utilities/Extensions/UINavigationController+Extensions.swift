c3772ddca264aca6e3172e6d8f701a5129f0d24f
Theo Sementa <theosementa@gmail.com>
2025-04-05 20:19:01 +0200

    feat: add swipe back

---
+++ Sources/NavigationKit/Utilities/Extensions/UINavigationController+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  NavigationKit
+//
+//  Created by Theo Sementa on 05/04/2025.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
