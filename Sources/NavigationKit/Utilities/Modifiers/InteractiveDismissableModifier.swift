335b61409f19f70526429388c990beb93b0e047d
Theo Sementa <theosementa@gmail.com>
2025-04-19 13:53:42 +0200

    feat: add intercative dismiss

---
+++ Sources/NavigationKit/Utilities/Modifiers/InteractiveDismissableModifier.swift
@@ -0,0 +1,52 @@
+//
+//  InteractiveDismissableView.swift
+//  NavigationKit
+//
+//  Created by Theo Sementa on 19/04/2025.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+private struct InteractiveDismissableView<T: View>: UIViewControllerRepresentable {
+    let view: T
+    let isDisabled: Bool
+    let onAttemptToDismiss: (() -> Void)?
+  
+    func makeUIViewController(context: Context) -> UIHostingController<T> {
+        UIHostingController(rootView: view)
+    }
+  
+    func updateUIViewController(_ uiViewController: UIHostingController<T>, context: Context) {
+        context.coordinator.dismissableView = self
+        uiViewController.rootView = view
+        uiViewController.parent?.presentationController?.delegate = context.coordinator
+    }
+  
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+  
+    class Coordinator: NSObject, UIAdaptivePresentationControllerDelegate {
+        var dismissableView: InteractiveDismissableView
+    
+        init(_ dismissableView: InteractiveDismissableView) {
+            self.dismissableView = dismissableView
+        }
+    
+        func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
+            !dismissableView.isDisabled
+        }
+    
+        func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+            dismissableView.onAttemptToDismiss?()
+        }
+    }
+}
+
+extension View {
+    public func interactiveDismissDisabled(_ isDisabled: Bool = true, onAttemptToDismiss: (() -> Void)? = nil) -> some View {
+        InteractiveDismissableView(view: self, isDisabled: isDisabled, onAttemptToDismiss: onAttemptToDismiss)
+    }
+}
