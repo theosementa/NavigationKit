7edcbaa346a06a324f0450ff60bcd560ed551c17
Theo Sementa <theosementa@gmail.com>
2025-12-29 09:07:01 +0100

    feat: update isolation

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,10 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "NavigationKit"
+            name: "NavigationKit",
+            swiftSettings: [
+                .defaultIsolation(MainActor.self)
+            ]
         )
     ]
 )
